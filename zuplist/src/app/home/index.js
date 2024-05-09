eb17b98f7d4d94bfee245cd7c9742a8c543663fb
zup-miau <sophiavarasquim987@gmail.com>
2024-05-09 11:02:55 -0300

    icons da 2 pégina

--- zuplist/src/app/home/index.js
+++ zuplist/src/app/home/index.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity
     } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
+import { styles } from './style';
 
 export default function HOME() {
   return (
@@ -17,7 +18,23 @@ export default function HOME() {
       resizeMode='repeat'
       style={{ flex: 1, justifyContent: 'flex-start'}}
       >
+      <View style={styles.header}>
+        <Text style={styles.title}> Lista de Produtos </Text>
+        <View>
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+      </View>
 
+      {/* listinha*/}
+      
+      <View style={styles.footer}>
+        <View style={styles.inputContainer}>
+          <TextInput />
+        </View>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Ionicons name="add" size={36} color="#fff" />
+        </TouchableOpacity>
+      </View>
       </ImageBackground>
     </SafeAreaView>
   )
---
+++ zuplist/src/app/home/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    header:{
+        padding: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#000000c0',
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#ffffff',
+    }
+});
